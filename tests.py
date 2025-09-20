671b8d70e3a3962ac40bf000985e73f6be0ecbf1
cheresna1991 <yaroslavcheg98@gmail.com>
2025-09-20 13:53:33 +0800

    падающий тест на решение уравнения с ожиданием двух целых чисел, результат: невозможность импортирования функции

--- tests.py
+++ tests.py
@@ -1,1 +1,8 @@
 #тесты, которые импортируются в основном коде
+from main import solve_quadratic
+
+def test_solve_quadratic_two_real_roots():
+    """Тест уравнения с двумя действительными корнями"""
+    result = solve_quadratic(1, -5, 6)
+    expected = (3.0, 2.0)
+    assert result == expected
